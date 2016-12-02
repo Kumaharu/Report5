5b4dc0f997b59d5af63eb20b2607fa895520f57c
Motoharu Kumanomido <e165720@.u-ryukyu.ac.jp>
2016-12-02 20:25:25 +0900

    nullpointexcpthon

--- src/main/jp/ac/uryukyu/ie/e165720/main.java
+++ src/main/jp/ac/uryukyu/ie/e165720/main.java
@@ -2,7 +2,16 @@ package jp.ac.uryukyu.ie.e165720;
 
 public class main {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+
+        } catch (Exception e) {
+            System.out.println("NullPointerExceptionが発生した");
+        }finally {
+            
+            System.out.println("スタックトレース");
+        }
     }
 }
+
