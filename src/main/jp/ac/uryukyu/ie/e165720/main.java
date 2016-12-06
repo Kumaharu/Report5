4717ec0dcf5d81dc0559015f7b4be0807aeb0fff
Motoharu Kumanomido <e165720@.u-ryukyu.ac.jp>
2016-12-06 16:03:51 +0900

    スタックトレース

--- src/main/jp/ac/uryukyu/ie/e165720/main.java
+++ src/main/jp/ac/uryukyu/ie/e165720/main.java
@@ -2,8 +2,12 @@ package jp.ac.uryukyu.ie.e165720;
 
 public class main {
     public static void main(String[] args) {
-            String str = "3．14";
-            double value = Double.parseDouble(str);
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionが起きた");
+            e.printStackTrace();
+        }
     }
 }
-
